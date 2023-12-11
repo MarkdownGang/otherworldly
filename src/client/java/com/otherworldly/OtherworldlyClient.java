8145fd1d8f142b1975d32b4a07e6f37c3bbc5d54
corrugatedbread <83032760+solirides@users.noreply.github.com>
2023-12-10 21:04:41 -0800

    import

---
+++ src/client/java/com/otherworldly/OtherworldlyClient.java
@@ -0,0 +1,10 @@
+package com.otherworldly;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class OtherworldlyClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
