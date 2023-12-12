8b4e29b879356edceb34c40a3acf4d959b631665
ColossalRenders <144396943+ColossalRenders@users.noreply.github.com>
2023-12-11 20:40:44 -0800

    Update Otherworldly.java

--- src/main/java/com/otherworldly/Otherworldly.java
+++ src/main/java/com/otherworldly/Otherworldly.java
@@ -5,11 +5,14 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.otherworldly.item.OtherworldlyItems;
+
 public class Otherworldly implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
     public static final Logger LOGGER = LoggerFactory.getLogger("otherworldly");
+	public static final String MOD_ID = "otherworldly";
 
 	@Override
 	public void onInitialize() {
@@ -18,5 +21,7 @@ public class Otherworldly implements ModInitializer {
 		// Proceed with mild caution.
 
 		LOGGER.info("Init Otherworldly");
+
+		OtherworldlyItems.registerItems();
 	}
 }
