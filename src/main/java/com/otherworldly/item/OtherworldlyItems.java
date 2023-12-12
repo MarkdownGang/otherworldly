a3f9c376205a05a8c6c792a191e912d1fe8b923f
ColossalRenders <144396943+ColossalRenders@users.noreply.github.com>
2023-12-11 21:02:25 -0800

    Added levitation_fruit to OtherworldlyItems.java

--- src/main/java/com/otherworldly/item/OtherworldlyItems.java
+++ src/main/java/com/otherworldly/item/OtherworldlyItems.java
@@ -11,7 +11,8 @@ import net.minecraft.util.Identifier;
 public class OtherworldlyItems {
     public static final Item TEST_ITEM = Registry.register(Registries.ITEM, new Identifier(Otherworldly.MOD_ID, "test_item"), new Item(new FabricItemSettings()));
     public static final Item OCTANT = Registry.register(Registries.ITEM, new Identifier(Otherworldly.MOD_ID, "octant"), new OctantItem(new FabricItemSettings()));
-
+    public static final Item LEVITATION_FRUIT = Registry.register(Registries.ITEM, new Identifier(Otherworldly.MOD_ID, "levitation_fruit"), new Item(new FabricItemSettings().food( new FoodComponent.Builder().hunger(10).saturationModifier(0.0f).statusEffect(new StatusEffectInstance(StatusEffects.LEVITATION, 400, 1), 1).build())));
+    
     public static void registerItems(){
         Otherworldly.LOGGER.debug("Registering items for Otherworldly");
     }
